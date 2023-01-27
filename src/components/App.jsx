dd82c31cf4a3da8a0493b10ade6ed07721724b45
Olena Bondar <olena.bondar1060@gmail.com>
2023-01-27 22:21:20 +0200

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,102 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+import FormContact from 'components/FormContact/FormContact';
+import Contacts from 'components/Contacts/Contacts';
+import Filter from 'components/Filter/Filter';
+import { Wrapper, Title } from './App.styled';
+
+class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    // const contactsParsed = JSON.parse(contacts);
+    if (contacts?.length) {
+      //contacts && contacts.length
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    // contacts !== prevState.contacts
+    if (contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  formSubmit = data => {
+    const normalizedName = data.name.toLowerCase();
+
+    const checkName = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === normalizedName
+    );
+
+    if (checkName) {
+      return alert(` ${data.name} is alredy in contacts`);
+    }
+    const checkNumber = this.state.contacts.find(
+      contact => contact.number === data.number
+    );
+    if (checkNumber) {
+      return alert(`${data.number} is already  in contacts`);
+    }
+
+    const id = nanoid();
+    const contact = { id, ...data };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  deleteContacts = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+
+    const lowerCaseFilterText = filter.toLowerCase();
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowerCaseFilterText)
+    );
+
+    return filteredContacts;
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <div>
+        <Wrapper>
+          <Title>Phonebook</Title>
+          <FormContact onSubmit={this.formSubmit} />
+
+          <Title>Contacts</Title>
+          <Filter filter={filter} onChange={this.changeFilter} />
+          <Contacts
+            contacts={this.getFilteredContacts()}
+            onChange={this.deleteContacts}
+          />
+        </Wrapper>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FormContact/FormContact.styled.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 32px;
+  padding: 16px;
+  max-width: 100%;
+  border: 1px solid #d1c2c2;
+  border-radius: 4px;
+  box-shadow: 2px 4px 10px #8f8f8f;
+  @media screen and (min-width: 480px) {
+    padding: 32px;
+  }
+`;
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+export const Input = styled.input`
+  padding: 8px 8px;
+  margin-top: 8px;
+  border: 1px solid #bba4a4;
+`;
+
+export const Button = styled.button`
+  padding: 8px 32px;
+  margin: 8px auto 0;
+
+  border: none;
+  border-radius: 4px;
+  color: #212121;
+  background-color: #c2ccd1;
+  font-size: 1em;
+  box-shadow: 1px 1px 5px #6c5656;
+
+  :hover,
+  :focus {
+    background-color: #0984b9;
+    color: white;
+    transition: all 0.2s;
+  }
+`;
